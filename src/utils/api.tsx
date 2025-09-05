56ca374e6aefa50d8cdeca256f019f79350094b1
Nathan <nathantrabson@gmail.com>
2025-09-04 21:30:51 -0300

    fix: corrigir URL base para o ambiente de teste

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -4,7 +4,7 @@ import { toast } from 'react-toastify';
 import * as TokenService from '@/app/authentication/auth.token';
 import { AuthService } from '@/app/authentication/auth.service';
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://lmtsteste23.ufape.edu.br';
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'https://lmtsteste22.ufape.edu.br/';
 const PUBLIC_ROUTES = [
   '/login',
   '/conta/criar-conta',
